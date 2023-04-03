f566c185313fcf9923b617e37c975248430e8aa1
savkin <godbryansk@yandex.ru>
2023-04-03 16:17:48 +0300

    Добавлены основные функции программы

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
 import openpyxl
 import datetime
 
-# Открываем исходный файл *.xlsx
-wb_source              = openpyxl.load_workbook('20230316.xlsx')
-sheet_source_bryansk   = wb_source.worksheets[1]
-sheet_source_abdc      = wb_source.worksheets[2]
-sheet_source_soop      = wb_source.worksheets[3]
-sheet_source_gibdd     = wb_source.worksheets[4]
-sheet_source_zapret    = wb_source.worksheets[5]
-sheet_source_zaderj    = wb_source.worksheets[6]
-sheet_source_zags      = wb_source.worksheets[7]
-
-# Создаем итоговый файл xlsx для копирования в него строк из исходного файла после фильтра
-wb_result              = openpyxl.Workbook()
-sheet_result_abdc      = wb_result.create_sheet("АБДЦ", 0)
-sheet_result_soop      = wb_result.create_sheet("АП СООП", 1)
-sheet_result_gibdd     = wb_result.create_sheet("АП ГИБДД", 2)
-sheet_result_zapret    = wb_result.create_sheet("ЗАПРЕТНИКИ", 3)
-sheet_result_zaderj    = wb_result.create_sheet("ЗАДЕРЖАНИЯ", 4)
-sheet_result_zags      = wb_result.create_sheet("ЗАГС", 5)
-
 # Проверка перед началом фильтра:
-"""
-print(f""           ВНИМАНИЕ! Перед обработкой таблица должна соответствовать следующим параметрам:
-    ---------------------------------------------------------------------------------------------------------------
+print(f"""                 ВНИМАНИЕ! Перед обработкой таблица должна соответствовать следующим параметрам:
+
+    ******************************************* ОФОРМЛЕНИЕ ТАБЛИЦЫ ************************************************
+    Содержать 8 страниц в порядке: [Адреса] [Брянск] [АБДЦ] [АП СООП] [АП ГИБДД] [ЗАПРЕТНИКИ] [ЗАДЕРЖАНИЯ] [ЗАГС]
+
+    ******************************************* ОФОРМЛЕНИЕ СТРАНИЦ ************************************************
     БРЯНСК:     Заголовок:      [Фамилия] [Имя] [Отчество] [Дата рождения (формат: ДД.ММ.ГГГГ)]
     ---------------------------------------------------------------------------------------------------------------
     АБДЦ:       Заголовок:      [Фамилия] [Имя] [Отчество] [Дата рождения (формат: ГГГГММДД)]
@@ -37,17 +21,73 @@ print(f""           ВНИМАНИЕ! Перед обработкой табли
     ЗАДЕРЖАНИЯ: 1 Строка:   [№] [Фамилия] [Имя] [Отчество] [Дата рождения (формат: ДД.ММ.ГГГГ)] (!!! ЗАГОЛОВКА НЕТ)
     ---------------------------------------------------------------------------------------------------------------
     ЗАГС:       Заголовок:  [№] [Фамилия] [Имя] [Отчество] [Дата рождения (формат: ДД.ММ.ГГГГ)]
-    ---------------------------------------------------------------------------------------------------------------
-"")
+""")
 
+source_file_name = str(input("Введите название файла для обработки (только формат xlsx): "))
 
-question = int(input("Если таблица соответсвтует указанным требованиям - введите цифру 1. "
-                     "Если нет, введите любой другой символ: "))
+# Если название файл введено без расширения - оно добавляется
+if ".xlsx" in source_file_name:
+    pass
+else:
+    source_file_name = source_file_name + ".xlsx"
 
+# Открываем исходный файл *.xlsx и получаем доступ к страницам
+try:
+    wb_source              = openpyxl.load_workbook(source_file_name)
+    sheet_source_bryansk   = wb_source.worksheets[1]
+    sheet_source_abdc      = wb_source.worksheets[2]
+    sheet_source_soop      = wb_source.worksheets[3]
+    sheet_source_gibdd     = wb_source.worksheets[4]
+    sheet_source_zapret    = wb_source.worksheets[5]
+    sheet_source_zaderj    = wb_source.worksheets[6]
+    sheet_source_zags      = wb_source.worksheets[7]
+except FileNotFoundError:
+    print("Введено неверное название файла или указанный файл отсутсует в текущей дирректории!")
+    input("")
+    exit()
+except IndexError:
+    input("Ошибка! В исходном файле не 8 страниц. Проверьте оформление файла!\n"
+          "Для выхода нажмите Enter!")
+    exit()
+
+# Создаем итоговый файл xlsx для копирования в него строк из исходного файла после фильтра
+wb_result              = openpyxl.Workbook()
+sheet_result_abdc      = wb_result.create_sheet("АБДЦ", 0)
+sheet_result_soop      = wb_result.create_sheet("АП СООП", 1)
+sheet_result_gibdd     = wb_result.create_sheet("АП ГИБДД", 2)
+sheet_result_zapret    = wb_result.create_sheet("ЗАПРЕТНИКИ", 3)
+sheet_result_zaderj    = wb_result.create_sheet("ЗАДЕРЖАНИЯ", 4)
+sheet_result_zags      = wb_result.create_sheet("ЗАГС", 5)
+
+# Получаем по 1 строке из кажой страницы для предварительного сравнения строк в страницах
+test_bryansk           = list(sheet_source_bryansk.iter_rows(min_row=2, min_col=1, max_col=4, values_only=True))[0]
+test_abdc              = list(sheet_source_abdc.iter_rows(   min_row=2, min_col=1, max_col=4, values_only=True))[0]
+test_soop              = list(sheet_source_soop.iter_rows(   min_row=1, min_col=1, max_col=4, values_only=True))[0]
+test_gibdd             = list(sheet_source_gibdd.iter_rows(  min_row=2, min_col=2, max_col=5, values_only=True))[0]
+test_zapret            = list(sheet_source_zapret.iter_rows( min_row=2, min_col=1, max_col=4, values_only=True))[0]
+test_zaderj            = list(sheet_source_zaderj.iter_rows( min_row=1, min_col=2, max_col=5, values_only=True))[0]
+test_zags              = list(sheet_source_zags.iter_rows(   min_row=2, min_col=2, max_col=5, values_only=True))[0]
+
+try:
+    print(f"""
+        ***************************** ПРЕДВАРИТЕЛЬНОЕ СРАВНЕНИЕ СТРОК В СТРАНИЦАХ *************************************
+        [Страница]\t\t[Фамилия] [Имя] [Отчество] [Дата рождения]
+        БРЯНСК:\t\t\t{test_bryansk[0]} {test_bryansk[1]} {test_bryansk[2]} {test_bryansk[3].strftime('%d.%m.%Y')}
+        АБДЦ:\t\t\t{test_abdc[0]} {test_abdc[1]} {test_abdc[2]} {str(test_abdc[3])[6:8]}.{str(test_abdc[3])[4:6]}.{str(test_abdc[3])[:4]}
+        АП СООП:\t\t{test_soop[0]} {test_soop[1]} {test_soop[2]} {test_soop[3].strftime('%d.%m.%Y')}
+        АП ГИБДД:\t\t{test_gibdd[0]} {test_gibdd[1]} {test_gibdd[2]} {test_gibdd[3]}
+        ЗАПРЕТНИКИ:\t\t{test_zapret[0]} {test_zapret[1]} {test_zapret[2]} {test_zapret[3].strftime('%d.%m.%Y')}
+        ЗАДЕРЖАНИЯ:\t\t{test_zaderj[0]} {test_zaderj[1]} {test_zaderj[2]} {test_zaderj[3].strftime('%d.%m.%Y')}
+        ЗАГС:\t\t\t{test_zags[0]} {test_zags[1]} {test_zags[2]} {test_zags[3]}
+    """)
+except AttributeError:
+    input("Исходный файл не соответвсует правилам оформления! Нажмите Enter для выхода.")
+    exit()
+
+question = int(input("Если таблица и строки в страницах соответсвтует указанным требованиям - введите цифру 1. "
+                     "Для выхода из программы введите любой другой символ: "))
 if question != 1:
     exit()
-"""
-
 
 # Фильтр страницы АБДЦ
 for row_abdc in sheet_source_abdc.iter_rows(min_row=2, values_only=True):
@@ -64,12 +104,13 @@ for row_abdc in sheet_source_abdc.iter_rows(min_row=2, values_only=True):
         if list_bryansk[3] != None:
             list_bryansk[3] = list_bryansk[3].strftime("%d.%m.%Y")
         #print('Bryansk', list_bryansk)
+
         # Если строка имеет совпадения по ФИО и дате рождения, то сохраяем эту строку в итоговый файл
         if (list_abdc[0] == list_bryansk[0] and
                 list_abdc[1] == list_bryansk[1] and
                 list_abdc[2] == list_bryansk[2] and
                 list_abdc[3] == list_bryansk[3]):
-            print(f"Совпадение АБДЦ с Брянск: {list_bryansk[0], list_bryansk[1], list_bryansk[2], list_bryansk[3]}")
+            print(f"Совпадение АБДЦ с Брянск: {list_bryansk}")
             sheet_result_abdc.append(row_abdc)
 
 # Фильтр страницы АП СООП
@@ -92,15 +133,15 @@ for row_soop in sheet_source_soop.iter_rows(min_row=1, values_only=True):
                 list_soop[1] == list_bryansk[1] and
                 list_soop[2] == list_bryansk[2] and
                 list_soop[3] == list_bryansk[3]):
-            print(f"Совпадение АП СООП с Брянск: {list_bryansk[0], list_bryansk[1], list_bryansk[2], list_bryansk[3]}")
+            print(f"Совпадение АП СООП с Брянск: {list_bryansk}")
             sheet_result_soop.append(row_soop)
 
 # Фильтр страницы АП ГИБДД
 for row_gibdd in sheet_source_gibdd.iter_rows(min_row=2, min_col=2, values_only=True):
     list_gibdd = [cell for cell in row_gibdd]
     # Приводим формат даты рождения к ДД.ММ.ГГГГ (только если поле не пустое)
-    if list_gibdd[3] != None:
-        list_gibdd[3] = list_gibdd[3]#.strftime("%d.%m.%Y")
+    #if list_gibdd[3] != None:
+    #    list_gibdd[3] = list_gibdd[3]#.strftime("%d.%m.%Y")
     #print('АП ГИБДД:', list_gibdd)
 
     # Сравниваем ФИО и дату рождения между АБДЦ и Брянск
@@ -115,7 +156,7 @@ for row_gibdd in sheet_source_gibdd.iter_rows(min_row=2, min_col=2, values_only=
                 list_gibdd[1] == list_bryansk[1] and
                 list_gibdd[2] == list_bryansk[2] and
                 list_gibdd[3] == list_bryansk[3]):
-            print(f"Совпадение АП ГИБДД с Брянск: {list_bryansk[0], list_bryansk[1], list_bryansk[2], list_bryansk[3]}")
+            print(f"Совпадение АП ГИБДД с Брянск: {list_bryansk}")
             sheet_result_gibdd.append(row_gibdd)
 
 # Фильтр страницы ЗАПРЕТНИКИ
@@ -138,9 +179,8 @@ for row_zapret in sheet_source_zapret.iter_rows(min_row=2, min_col=1, values_onl
                 list_zapret[1] == list_bryansk[1] and
                 list_zapret[2] == list_bryansk[2] and
                 list_zapret[3] == list_bryansk[3]):
-            print(f"Совпадение ЗАПРЕТНИКИ с Брянск: {list_bryansk[0], list_bryansk[1], list_bryansk[2], list_bryansk[3]}")
+            print(f"Совпадение ЗАПРЕТНИКИ с Брянск: {list_bryansk}")
             sheet_result_zapret.append(row_zapret)
-
 
 # Фильтр страницы ЗАДЕРЖАНИЯ
 for row_zaderj in sheet_source_zaderj.iter_rows(min_row=1, min_col=2, values_only=True):
@@ -162,7 +202,7 @@ for row_zaderj in sheet_source_zaderj.iter_rows(min_row=1, min_col=2, values_onl
                 list_zaderj[1] == list_bryansk[1] and
                 list_zaderj[2] == list_bryansk[2] and
                 list_zaderj[3] == list_bryansk[3]):
-            print(f"Совпадение ЗАПРЕТНИКИ с Брянск: {list_bryansk[0], list_bryansk[1], list_bryansk[2], list_bryansk[3]}")
+            print(f"Совпадение ЗАПРЕТНИКИ с Брянск: {list_bryansk}")
             sheet_result_zaderj.append(row_zaderj)
 
 # Фильтр страницы ЗАГС
@@ -185,8 +225,17 @@ for row_zags in sheet_source_zags.iter_rows(min_row=2, min_col=2, values_only=Tr
                 list_zags[1] == list_bryansk[1] and
                 list_zags[2] == list_bryansk[2] and
                 list_zags[3] == list_bryansk[3]):
-            print(f"Совпадение ЗАПРЕТНИКИ с Брянск: {list_bryansk[0], list_bryansk[1], list_bryansk[2], list_bryansk[3]}")
+            print(f"Совпадение ЗАПРЕТНИКИ с Брянск: {list_bryansk}")
             sheet_result_zags.append(row_zags)
 
 
-wb_result.save('result.xlsx')
+result_file_name = "result_" + source_file_name
+
+try:
+    wb_result.save(result_file_name)
+except PermissionError:
+    input("\nОшибка сохранения итогового файла. Возможно предыдущая версия файла уже открыта, закртойте его!\n"
+          "Для выхода нажмите Enter!")
+    exit()
+
+input("Для завершения программы нажмите Enter ")
